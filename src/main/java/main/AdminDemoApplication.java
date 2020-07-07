f29ab2d70c9003bb58c0c2778429c878410e39df
JiangNinShang <1753291617@qq.com>
2020-07-07 17:20:10 +0800

    test

---
+++ src/main/java/main/AdminDemoApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDemoApplication.class, args);
+	}
+
+}
